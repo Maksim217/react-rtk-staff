ca283b4094d6490dcf5dfb0cd465f5f2ea43029e
mgandronov <mgandronov2017@gmail.com>
2022-07-09 16:39:09 +0500

    добавляет файл с тестовыми данными

---
+++ src/mock.ts
@@ -0,0 +1,16 @@
+import { IControlItem } from './models/IControlItem';
+
+export const positions: IControlItem[] = [
+  { label: 'Fronend разработчик', value: 'Fronend developer' },
+  { label: 'Backend разработчик', value: 'Backend developer' },
+  { label: 'Тестировщик', value: 'Tester' },
+  { label: 'Руководитель группы', value: 'Team lead' },
+  { label: 'Менеджер проектов', value: 'Project Manager' },
+  { label: 'Администратор', value: 'Administrator' },
+  { label: 'HR менеджер', value: 'HR manager' },
+];
+
+export const sex: IControlItem[] = [
+  { label: 'Мужской', value: 'male' },
+  { label: 'Женский', value: 'female' },
+];
