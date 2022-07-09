f94068e5ad9490fc6d91c500dbe06d6f36789ce4
mgandronov <mgandronov2017@gmail.com>
2022-07-09 16:40:29 +0500

    добавляет файл с константами

---
+++ src/const.ts
@@ -0,0 +1,12 @@
+export enum Reducer {
+  Staff = 'STAFF',
+}
+
+export enum Storage {
+  Staff = 'staff',
+}
+
+export type Order = 'asc' | 'desc';
+
+export const imgSrc =
+  'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png';
