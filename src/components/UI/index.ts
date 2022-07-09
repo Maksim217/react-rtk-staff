118c6d558de37a13dd1f44d322784fd8b308ddcc
mgandronov <mgandronov2017@gmail.com>
2022-07-09 17:37:31 +0500

    добавляет файл index.ts для экспорта пользовательских UI компонентов

---
+++ src/components/UI/index.ts
@@ -0,0 +1,7 @@
+import Radio from './Radio';
+import Input from './Input';
+import Select from './Select';
+import Checkbox from './Checkbox';
+import DatePicker from './DatePicker';
+
+export { Input, Radio, Select, Checkbox, DatePicker };
