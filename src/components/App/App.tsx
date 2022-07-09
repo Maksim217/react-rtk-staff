fb07783ffa2fa7959e17ec256c5602134121f911
mgandronov <mgandronov2017@gmail.com>
2022-07-09 17:52:52 +0500

    добавляет главный компонент приложения

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Staff } from '../../pages';
+
+const App: React.FC = (): React.ReactElement => {
+  return (
+    <>
+      <Staff />
+    </>
+  );
+};
+
+export default App;
