c8a99b7d95c873581f00db34b5c573fa9dfc1412
mgandronov <mgandronov2017@gmail.com>
2022-07-09 17:38:24 +0500

    добавляет файл index.ts для экспорта пользовательских компонентов

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import App from './App';
+import Toolbar from './Toolbar';
+import AddForm from './AddForm';
+import UpdateForm from './UpdateForm';
+import EnhancedTable from './EnhancedTable';
+import EnhancedTableHead from './EnhancedTableHead';
+
+export { App, Toolbar, AddForm, UpdateForm, EnhancedTable, EnhancedTableHead };
